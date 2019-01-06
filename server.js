3ea02e510c19acf4731ee7a31a6e673337e0adfc
Szabó <szabogergely1528@gmail.com>
2019-01-06 12:23:08 +0100

    mangoose beállítás

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { ApolloServer} from 'apollo-server-express';
+import { ApolloServer } from 'apollo-server-express';
 import typeDefs from './graphql/schema';
 import resolvers from './graphql/resolvers';
 import mongoose from 'mongoose';
@@ -9,6 +9,12 @@ const apollo = new ApolloServer({ typeDefs, resolvers });
 const users = [];
 apollo.applyMiddleware({ app })
 
-app.listen({ port: 4000 }, () =>
+mongoose.connect('mongodb://elynea:Narzie2012rohaN@ds137019.mlab.com:37019/keringerteam')
+.then(() => {
+  app.listen({ port: 4000 }, () =>
   console.log(`🚀 Server ready at http://localhost:4000`)
 )
+})
+.catch(err => {
+  console.log(err) });
+
