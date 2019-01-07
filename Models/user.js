eed31b79f560316ec0659001414c6777d6d9ea27
Szabó <szabogergely1528@gmail.com>
2019-01-07 16:42:06 +0100

    User adatbázis beállítása

--- Models/user.js
+++ Models/user.js
@@ -1,17 +1,51 @@
-import mongoose from 'mongoose';
+import mongoose from 'mongoose'
 
-const Schema = mongoose.Schema;
+const userSchema = new mongoose.Schema({
+  // A felhasználó Email címe
+  email: { 
+    type: String,
+    required: true
+  },
+  // A felhasználó jelszava
+  password: {
+    type: String,
+    required: true
+  },
+  // A felhasználó vezetékneve
+  firstname: {
+    type: Number,
+    required: true
+  },
+  // A felhasználó keresztneve
+  lastname: {
+    type: String,
+    required: true
+  },
+  // A felhasználót kétrehozó felhasználó (vezetéknév + keresztnév)
+  creator: {
+    type: String,
+    required: true
+  },
+  // A felhasználó létrehozásának dátuma
+  created: {
+    type: Date,
+    required: true
+  },
+  // A felhasználót változtató felhasználó (vezetéknév + keresztnév)
+  updater: {
+    type: String,
+    required: false,
+    default : null
+  },
+  // A felhasználó változtatásának dátuma
+  updated: {
+    type: Date,
+    required: false,
+    default : null
+  }
 
-const userSchema = new Schema({
-    email: {
-        type: String,
-        required: true
-
-    },
-    password: {
-        type: String,
-        required: true
-    }
 });
 
-module.exports = mongoose.model('User', userSchema);
+const User = mongoose.model('User', userSchema);
+
+export default User
