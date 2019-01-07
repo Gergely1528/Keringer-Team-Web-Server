edf7eb2db9fdf32bcfec5614b42ca7f1dfc32e3d
Szabó <szabogergely1528@gmail.com>
2019-01-07 17:00:28 +0100

    User GraphQL séma létrehozása

--- graphql/schema/user.js
+++ graphql/schema/user.js
@@ -1,19 +1,43 @@
-export const types = `
-  type User {
-    id: ID!
-    email: String!
-    password: String!
-  }
-`;
+import { gql } from 'apollo-server-express';  //gql típus importálása
 
-export const queries = `
-  teszt: String
-  hello: String
-  getUser(email: String!): User 
-  allUser: [User!]!
-`;
-
-export const mutations = `
-  createUser(email: String!, password: String!): User
-
-`;
+export default gql`
+"""Felhasználók."""
+type User {
+    """A felhasználó egyedi azonosítója."""
+    id: ID
+    """A felhasználó Email címe."""
+    email: String! 
+    """ A felhasználó jelszava."""
+    password: String! 
+    """ A felhasználó vezetékneve."""
+    firstname: String! 
+    """ A felhasználó keresztneve."""
+    lastname: String!  
+    """ A felhasználót kétrehozó felhasználó (vezetéknév + keresztnév)."""
+    creator: String!
+    """ A felhasználó létrehozásának dátuma."""
+    created: String! 
+    """ A felhasználót változtató felhasználó (vezetéknév + keresztnév)."""
+    updater: String
+    """ A felhasználó változtatásának dátuma."""
+    updated: String 
+}
+"""Felhasználók típus lekérdezései"""
+type Query {
+    """Aktuális felhasználó lekérdezése."""
+    readUser: User!
+    """Az összes felhasználó lekérdezése."""
+    readUsers: [User!]!
+} 
+"""Felhasználók típus mutációi"""    
+type Mutation {
+    """Felhasználó létrehozása"""  
+    createUser(email: String password: String, firstname: String, lastname: String, creator: String, created: String, updater: String, updated: String): User
+    """Felhasználó módosítása"""  
+    updateUser(email: String, password: String, firstname: String, lastname: String, creator: String, created: String, updater: String, updated: String): User
+    """Felhasználó törlése"""  
+    deleteUser(firstname: String, lastname: String): User
+    """Felhasználói belépés""" 
+    loginUser(email:String, password: String): Token
+}
+`
