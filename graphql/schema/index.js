2668d834dbe6f00cc62cbe666c2521c0f5ceb268
Szabó <szabogergely1528@gmail.com>
2019-01-07 17:03:12 +0100

    Graphql index fálj user, token permissin sémák hozzáadása

--- graphql/schema/index.js
+++ graphql/schema/index.js
@@ -1,35 +1,9 @@
-import * as User from './user';
-import * as Roles from './roles';
+import user from './user';
+import token from './token';
+import permission from './permissions';
 
-import { gql } from 'apollo-server-express';
-
-const types = [];
-const queries = [];
-const mutations = [];
-
-const schemas = [User, Roles];
-
-schemas.forEach((s) => {
-  types.push(s.types);
-  queries.push(s.queries);
-  mutations.push(s.mutations);
-});
-
-export default  gql`
-
-
-  ${types.join('\n')}
-
-  type Query {
-    ${queries.join('\n')}
-  }
-
-  type Mutation {
-    ${mutations.join('\n')}
-  }
-
-  schema {
-    query: Query
-    mutation: Mutation
-  }
-`;
+export default [
+  user,
+  token,
+  permission
+]
