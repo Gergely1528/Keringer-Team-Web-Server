6cd92f5390c3dc30d8ef9ed81899791d012e165d
Szabó <szabogergely1528@gmail.com>
2019-01-08 10:39:40 +0100

    Permissions adatbázi létrehozása

--- Models/permission.js
+++ Models/permission.js
@@ -1,40 +1,52 @@
 import mongoose from 'mongoose' //mongoose importálása
-// Adatbázis a felhasználói jogosultságok tárolására
+
+//   Adatbázis a felhasználói jogosultságok tárolására   
+ 
 const permissionSchema = new mongoose.Schema({
-  // Felhasználói belépés
-  loginUser: { 
-    type: Boolean,
+  // A felhasználó email címe a jogosultságok rendszerezéséhez.
+  email: {
+    type: String,
     required: false
   },
-  // Felhasználói adatok olvasása.
-  readUser: { 
+  // Jogosultság a saját felhasználói adatainak lekérdezésére.
+  readUser: {
     type: Boolean,
-    required: false
+    required: false,
+    default: false
   },
-  // Aktuális felhasználó lekérdezése.
-  readUser: { 
+  // Jogosultság az összes felhasználó adatainak lekérdezésére.
+  readUsers: {
     type: Boolean,
-    required: false
-  },// Az összes felhasználó lekérdezése.
-  readUsers: { 
+    required: false,
+    default: false
+  },
+  // Jogosultság új felhasználó létrehozására.
+  createUser: {
     type: Boolean,
-    required: false
-  },// Felhasználó létrehozása.
-  createUser: { 
+    required: false,
+    default: false
+  },
+  // Jogosultság a felhasználói adatok módosítására.
+  updateUser: {
     type: Boolean,
-    required: false
-  },// Felhasználó módosítása.
-  updateUser: { 
+    required: false,
+    default: false
+  },
+  // Jogosultság felhasználó törlésére.
+  deleteUser: {
     type: Boolean,
-    required: false
-  },// Felhasználó törlése.
-  deleteUser: { 
+    required: false,
+    default: false
+  },
+  // Belépési engedély a rendszerbe.
+  loginUser: {
     type: Boolean,
-    required: false
-  }// Felhasználói belépés.
-  
+    required: false,
+    default: false
+  }
+
 });
 
-const User = mongoose.model('Permission', permissionSchema);
+const Permission = mongoose.model('Permission', permissionSchema);
 
 export default Permission
